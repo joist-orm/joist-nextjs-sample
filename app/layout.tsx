24f57a319c09adbf1dfa81db3ed01409fe14b285
Stephen Haberman <stephen.haberman@gmail.com>
2024-04-07 20:04:06 -0500

    Simplify.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,9 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-prisma.vercel.app'),
-  title: 'Vercel Postgres Demo with Prisma',
+  title: 'Next.js Postgres Demo with Prisma',
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
+    'A simple Next.js app with Postgres as the database and Joist as the ORM',
 }
 
 const inter = Inter({
