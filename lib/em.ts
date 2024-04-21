0a6f180904f912644c3efb2f4bb4e3d0a4c98390
Stephen Haberman <stephen.haberman@gmail.com>
2024-04-21 13:05:01 -0500

    Just use a global const, prisma style.

--- lib/em.ts
+++ lib/em.ts
@@ -4,19 +4,28 @@ import { JsonAggregatePreloader } from "joist-plugin-join-preloading";
 import { knex as createKnex } from "knex";
 import { cache } from "react";
 
-/** Cache each server-side request's `knex` instance. */
-const getKnex = cache(() =>
-  createKnex({
+function newDriver() {
+  const knex = createKnex({
     client: "pg",
     connection: newPgConnectionConfig() as any,
     debug: true,
     asyncStackTraces: true,
-  }),
-);
+  });
+  return new PostgresDriver(knex);
+}
+
+declare global {
+  var globalDriver: undefined | PostgresDriver;
+}
+
+const driver = globalThis.globalDriver ?? newDriver();
+if (process.env.NODE_ENV !== "production") globalThis.globalDriver = driver;
+
+// This will be cached pre-request I guess?
+const preloadPlugin = new JsonAggregatePreloader();
+const em = new EntityManager({}, { driver, preloadPlugin });
 
 /** Cache each server-side request's `EntityManager` instance. */
-export const getEm = cache(() => {
-  const driver = new PostgresDriver(getKnex());
-  const preloadPlugin = new JsonAggregatePreloader();
-  return new EntityManager({}, { driver, preloadPlugin });
-});
+export function getEm() {
+  return em;
+}
